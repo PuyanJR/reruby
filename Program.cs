eabd0a0100a5dd68302b621ff214fc923c35d3b0
David <David@DESKTOP-PTDC5F6>
2025-12-06 15:20:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+
+partial class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Universal Currency Generator! (UCG) by MirkoKING https://youtube.com/c/MirkoKING");
+        Thread.Sleep(1000);
+        Console.WriteLine("Searching for updates...");
+        Thread.Sleep(5000);
+        Console.WriteLine("Running latest version! 4.8.5");
+        Console.WriteLine("Waiting for RobloxPlayerBeta.exe...");
+
+        while (true)
+        {
+            Process[] processes = Process.GetProcessesByName("RobloxPlayerBeta");
+            // Some systems use "RobloxPlayerBeta" instead of "roblox"
+
+            if (processes.Length > 0)
+            {
+                Console.WriteLine("Roblox started!");
+                break; // Continue your program here
+            }
+
+            Thread.Sleep(500); // reduce CPU usage
+        }
+
+        // Continue your program logic here...
+        Console.WriteLine("Program is continuing...");
+    }
+}
+
